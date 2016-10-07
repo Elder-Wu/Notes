da07eddfb9c479d374db572c155741440e62bb20
wuming <itiswm@163.com>
2016-10-07 23:15:11 +0800

    连按两次Back键退出应用程序

--- customview/src/main/java/com/example/customview/MainActivity.java
+++ customview/src/main/java/com/example/customview/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.customview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long lastTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastTime < 2 * 1000) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(this, "请再按一次", Toast.LENGTH_SHORT).show();
+            lastTime = currentTime;
+        }
     }
 }
