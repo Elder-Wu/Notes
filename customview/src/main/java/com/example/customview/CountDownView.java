bc3b8c3ca24a3783d761a8cde020bb80d4c4847a
wuming <itiswm@163.com>
2016-10-09 21:39:56 +0800

    自定义倒计时的广告标记，模仿“迅雷”手机客户端

--- customview/src/main/java/com/example/customview/CountDownView.java
+++ customview/src/main/java/com/example/customview/CountDownView.java
@@ -1,14 +1,13 @@
 package com.example.customview;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.os.Build;
 import android.os.CountDownTimer;
+import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -17,13 +16,21 @@ import android.view.View;
 
 public class CountDownView extends View {
 
-    private static final int BORDER_WIDTH = 10;
+    private static final String TAG = CountDownView.class.getSimpleName();
+    private static final int BACKGROUND_COLOR = 0x50555555;
+    private static final int BORDER_WIDTH = 15;
+    private static final int BORDER_COLOR = 0xFF6ADBFE;
+    private static final int TEXT_SIZE = 50;
+    private static final int TEXT_COLOR = 0xFFFFFFFF;
+
 
     private Paint circlePaint;
-    private Paint textPaint;
+    private TextPaint textPaint;
     private Paint borderPaint;
 
     private float progress;
+
+    private CountDownTimerListener listener;
 
     public CountDownView(Context context) {
         this(context, null);
@@ -38,24 +45,22 @@ public class CountDownView extends View {
         circlePaint = new Paint();
         circlePaint.setAntiAlias(true);
         circlePaint.setDither(true);
-        circlePaint.setColor(Color.GRAY);
+        circlePaint.setColor(BACKGROUND_COLOR);
         circlePaint.setStyle(Paint.Style.FILL);
 
-        textPaint = new Paint();
+        textPaint = new TextPaint();
         textPaint.setAntiAlias(true);
         textPaint.setDither(true);
-        textPaint.setColor(Color.WHITE);
-        textPaint.setTextSize(40);
+        textPaint.setColor(TEXT_COLOR);
+        textPaint.setTextSize(TEXT_SIZE);
         textPaint.setTextAlign(Paint.Align.CENTER);
 
         borderPaint = new Paint();
         borderPaint.setAntiAlias(true);
         borderPaint.setDither(true);
-        borderPaint.setColor(Color.DKGRAY);
+        borderPaint.setColor(BORDER_COLOR);
         borderPaint.setStrokeWidth(BORDER_WIDTH);
         borderPaint.setStyle(Paint.Style.STROKE);
-
-        progress = 50;
     }
 
     @Override
@@ -65,31 +70,52 @@ public class CountDownView extends View {
         int min = Math.min(width, height);
         //画底盘
         canvas.drawCircle(width / 2, height / 2, min / 2, circlePaint);
+
         //画边框
+        RectF rectF;
         if (width > height) {
-            RectF rectF = new RectF(width / 2 - min / 2, 0, width / 2 + min / 2, height);
-            canvas.drawArc(rectF, 0, progress, true, borderPaint);
+            rectF = new RectF(width / 2 - min / 2 + BORDER_WIDTH / 2, 0 + BORDER_WIDTH / 2, width / 2 + min / 2 - BORDER_WIDTH / 2, height - BORDER_WIDTH / 2);
         } else {
-            RectF rectF = new RectF(0, height / 2 - min / 2, width, height / 2 - min / 2);
-            canvas.drawArc(rectF, 0, progress, true, borderPaint);
+            rectF = new RectF(BORDER_WIDTH / 2, height / 2 - min / 2 + BORDER_WIDTH / 2, width - BORDER_WIDTH / 2, height / 2 - BORDER_WIDTH / 2 + min / 2);
         }
+        canvas.drawArc(rectF, -90, progress, false, borderPaint);
+
         //画居中的文字
-        canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent() + textPaint.getTextSize() / 2, textPaint);
+//        canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent() + textPaint.getTextSize() / 2, textPaint);
+        canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent(), textPaint);
     }
 
     public void start() {
-        CountDownTimer countDownTimer = new CountDownTimer(3600, 60) {
+        CountDownTimer countDownTimer = new CountDownTimer(3600, 36) {
             @Override
             public void onTick(long millisUntilFinished) {
-                progress = (3600 - millisUntilFinished) / 3600 * 360;
+                progress = ((3600 - millisUntilFinished) / 3600f) * 360;
+                Log.d(TAG, "progress:" + progress);
                 invalidate();
             }
 
             @Override
             public void onFinish() {
-                progress = 0;
+                progress = 360;
                 invalidate();
+                if (listener != null) {
+                    listener.onFinishCount();
+                }
             }
-        };
+        }.start();
+        if (listener != null) {
+            listener.onStartCount();
+        }
+    }
+
+    public void setCountDownTimerListener(CountDownTimerListener listener) {
+        this.listener = listener;
+    }
+
+    public interface CountDownTimerListener {
+
+        void onStartCount();
+
+        void onFinishCount();
     }
 }
