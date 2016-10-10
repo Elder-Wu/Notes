e872b72ab2f81e4070f8ff0b6434c8dc83f4116d
wuming <itiswm@163.com>
2016-10-10 21:56:23 +0800

    自定义倒计时View制作完成，回家睡觉

--- customview/src/main/java/com/example/customview/CountDownView.java
+++ customview/src/main/java/com/example/customview/CountDownView.java
@@ -1,10 +1,13 @@
 package com.example.customview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.CountDownTimer;
+import android.text.Layout;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,17 +21,25 @@ public class CountDownView extends View {
 
     private static final String TAG = CountDownView.class.getSimpleName();
     private static final int BACKGROUND_COLOR = 0x50555555;
-    private static final int BORDER_WIDTH = 15;
+    private static final float BORDER_WIDTH = 15f;
     private static final int BORDER_COLOR = 0xFF6ADBFE;
-    private static final int TEXT_SIZE = 50;
+    private static final String TEXT = "跳过广告";
+    private static final float TEXT_SIZE = 50f;
     private static final int TEXT_COLOR = 0xFFFFFFFF;
 
+    private int backgroundColor;
+    private float borderWidth;
+    private int borderColor;
+    private String text;
+    private int textColor;
+    private float textSize;
 
     private Paint circlePaint;
     private TextPaint textPaint;
     private Paint borderPaint;
 
-    private float progress;
+    private float progress = 135;
+    private StaticLayout staticLayout;
 
     private CountDownTimerListener listener;
 
@@ -38,6 +49,17 @@ public class CountDownView extends View {
 
     public CountDownView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CountDownView);
+        backgroundColor = ta.getColor(R.styleable.CountDownView_background_color, BACKGROUND_COLOR);
+        borderWidth = ta.getDimension(R.styleable.CountDownView_border_width, BORDER_WIDTH);
+        borderColor = ta.getColor(R.styleable.CountDownView_border_color, BORDER_COLOR);
+        text = ta.getString(R.styleable.CountDownView_text);
+        if (text == null) {
+            text = TEXT;
+        }
+        textSize = ta.getDimension(R.styleable.CountDownView_text_size, TEXT_SIZE);
+        textColor = ta.getColor(R.styleable.CountDownView_text_color, TEXT_COLOR);
+        ta.recycle();
         init();
     }
 
@@ -45,22 +67,40 @@ public class CountDownView extends View {
         circlePaint = new Paint();
         circlePaint.setAntiAlias(true);
         circlePaint.setDither(true);
-        circlePaint.setColor(BACKGROUND_COLOR);
+        circlePaint.setColor(backgroundColor);
         circlePaint.setStyle(Paint.Style.FILL);
 
         textPaint = new TextPaint();
         textPaint.setAntiAlias(true);
         textPaint.setDither(true);
-        textPaint.setColor(TEXT_COLOR);
-        textPaint.setTextSize(TEXT_SIZE);
+        textPaint.setColor(textColor);
+        textPaint.setTextSize(textSize);
         textPaint.setTextAlign(Paint.Align.CENTER);
 
         borderPaint = new Paint();
         borderPaint.setAntiAlias(true);
         borderPaint.setDither(true);
-        borderPaint.setColor(BORDER_COLOR);
-        borderPaint.setStrokeWidth(BORDER_WIDTH);
+        borderPaint.setColor(borderColor);
+        borderPaint.setStrokeWidth(borderWidth);
         borderPaint.setStyle(Paint.Style.STROKE);
+
+        int textWidth = (int) textPaint.measureText(text.substring(0, (text.length() + 1) / 2));
+        staticLayout = new StaticLayout(text, textPaint, textWidth, Layout.Alignment.ALIGN_NORMAL, 1F, 0, false);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        if (widthMode != MeasureSpec.EXACTLY) {
+            width = staticLayout.getWidth();
+        }
+        if (heightMode != MeasureSpec.EXACTLY) {
+            height = staticLayout.getHeight();
+        }
+        setMeasuredDimension(width, height);
     }
 
     @Override
@@ -70,22 +110,24 @@ public class CountDownView extends View {
         int min = Math.min(width, height);
         //画底盘
         canvas.drawCircle(width / 2, height / 2, min / 2, circlePaint);
-
         //画边框
         RectF rectF;
         if (width > height) {
-            rectF = new RectF(width / 2 - min / 2 + BORDER_WIDTH / 2, 0 + BORDER_WIDTH / 2, width / 2 + min / 2 - BORDER_WIDTH / 2, height - BORDER_WIDTH / 2);
+            rectF = new RectF(width / 2 - min / 2 + borderWidth / 2, 0 + borderWidth / 2, width / 2 + min / 2 - borderWidth / 2, height - borderWidth / 2);
         } else {
-            rectF = new RectF(BORDER_WIDTH / 2, height / 2 - min / 2 + BORDER_WIDTH / 2, width - BORDER_WIDTH / 2, height / 2 - BORDER_WIDTH / 2 + min / 2);
+            rectF = new RectF(borderWidth / 2, height / 2 - min / 2 + borderWidth / 2, width - borderWidth / 2, height / 2 - borderWidth / 2 + min / 2);
         }
         canvas.drawArc(rectF, -90, progress, false, borderPaint);
-
         //画居中的文字
-//        canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent() + textPaint.getTextSize() / 2, textPaint);
-        canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent(), textPaint);
+//       canvas.drawText("稍等片刻", width / 2, height / 2 - textPaint.descent() + textPaint.getTextSize() / 2, textPaint);
+        canvas.translate(width / 2, height / 2 - staticLayout.getHeight() / 2);
+        staticLayout.draw(canvas);
     }
 
     public void start() {
+        if (listener != null) {
+            listener.onStartCount();
+        }
         CountDownTimer countDownTimer = new CountDownTimer(3600, 36) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -103,9 +145,6 @@ public class CountDownView extends View {
                 }
             }
         }.start();
-        if (listener != null) {
-            listener.onStartCount();
-        }
     }
 
     public void setCountDownTimerListener(CountDownTimerListener listener) {
