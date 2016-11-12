15536805eff6f164cf4f218a62fa2bd1eec9ad50
wuming <itiswm@163.com>
2016-11-12 12:50:31 +0800

    优化了ImageBanner的代码

--- notes/src/main/java/com/wuzhanglao/niubi/fragment/FragmentViewPager.java
+++ notes/src/main/java/com/wuzhanglao/niubi/fragment/FragmentViewPager.java
@@ -21,18 +21,18 @@ public class FragmentViewPager extends BaseFragment {
         return R.layout.fragment_viewpager;
     }
 
-@Override
-public void initView(View view, @Nullable Bundle savedInstanceState) {
-    banner = (ImageBanner) view.findViewById(R.id.fragment_viewpager_imagebanner);
-    banner.addImage(getImageView(R.drawable.mv1));
-    banner.addImage(getImageView(R.drawable.mv2));
-    banner.addImage(getImageView(R.drawable.mv3));
-    banner.addImage(getImageView(R.drawable.mv4));
-}
+    @Override
+    public void initView(View view, @Nullable Bundle savedInstanceState) {
+        banner = (ImageBanner) view.findViewById(R.id.fragment_viewpager_imagebanner);
+        banner.addImage(getImageView(R.drawable.mv1));
+        banner.addImage(getImageView(R.drawable.mv2));
+        banner.addImage(getImageView(R.drawable.mv3));
+        banner.addImage(getImageView(R.drawable.mv4));
+    }
 
-private ImageView getImageView(int resId) {
-    ImageView image = new ImageView(context);
-    image.setImageResource(resId);
-    return image;
-}
+    private ImageView getImageView(int resId) {
+        ImageView image = new ImageView(context);
+        image.setImageResource(resId);
+        return image;
+    }
 }
