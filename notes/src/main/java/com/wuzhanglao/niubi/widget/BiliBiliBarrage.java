3c8501d121c98f6c42cdbeda0fddb44ef8729f26
wuming <itiswm@163.com>
2016-11-29 23:20:02 +0800

    哔哩哔哩弹幕控件开始制作

---
+++ notes/src/main/java/com/wuzhanglao/niubi/widget/BiliBiliBarrage.java
@@ -0,0 +1,19 @@
+package com.wuzhanglao.niubi.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by wuming on 2016/11/28.
+ */
+
+public class BiliBiliBarrage extends RelativeLayout {
+    public BiliBiliBarrage(Context context) {
+        super(context);
+    }
+
+    public BiliBiliBarrage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
