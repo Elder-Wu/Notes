6adc3f5286822b3688e814e6fa458a5e99a751ce
wuming <itiswm@163.com>
2016-12-07 13:31:31 +0800

    删掉了调试代码

--- notes/src/main/java/com/wuzhanglao/niubi/activity/MainActivity.java
+++ notes/src/main/java/com/wuzhanglao/niubi/activity/MainActivity.java
@@ -118,7 +118,6 @@ public class MainActivity extends ToolbarActivity implements TextHolderAdatpter.
 
     @Override
     public void onTextClick(TextBean bean) {
-        UIUtils.showToast(getPackageName());
         RxBus.send(new Random().nextInt());
         if (UIUtils.isDoubleClick()) {
             return;
