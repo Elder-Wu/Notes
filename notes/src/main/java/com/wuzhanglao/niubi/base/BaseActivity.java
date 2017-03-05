63e0f2f5b5509406a820fee4ca3097239b9b86a3
wuming <wuming@wumingdeMacBook-Pro.local>
2017-03-05 23:05:16 +0800

    use rx-lifecycle

--- notes/src/main/java/com/wuzhanglao/niubi/base/BaseActivity.java
+++ notes/src/main/java/com/wuzhanglao/niubi/base/BaseActivity.java
@@ -1,23 +1,22 @@
 package com.wuzhanglao.niubi.base;
 
-import android.support.v7.app.AppCompatActivity;
-
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 import com.umeng.analytics.MobclickAgent;
 
 /*
  * Created by wuming on 16/10/13.
  */
 
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends RxAppCompatActivity {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        MobclickAgent.onResume(this);
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		MobclickAgent.onResume(this);
+	}
 
-    public void onPause() {
-        super.onPause();
-        MobclickAgent.onPause(this);
-    }
+	public void onPause() {
+		super.onPause();
+		MobclickAgent.onPause(this);
+	}
 }
